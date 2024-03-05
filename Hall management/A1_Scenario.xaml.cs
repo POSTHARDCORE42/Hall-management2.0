9f1182ccbeaf40989ccca4046f08fe9866b7965c
gorokhov_de <gorokhov_de@G-A1-DELL>
2024-03-05 20:19:13 +0300

    Добавьте файлы проекта.

---
+++ Hall management/A1_Scenario.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hall_management
+{
+    /// <summary>
+    /// Логика взаимодействия для A1_Scenario.xaml
+    /// </summary>
+    public partial class A1_Scenario : Window
+    {
+        public A1_Scenario()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Устанавливаем размер масштаба в процентах
+            double scalePercentage = 100; 
+            SetWebBrowserZoom(webBrowser1, scalePercentage);
+            SetWebBrowserZoom(webBrowser2, scalePercentage);
+        }
+
+        private void SetWebBrowserZoom(WebBrowser webBrowser, double scalePercentage)
+        {
+            // Формируем скрипт для изменения масштаба страницы
+            string script = $"document.body.style.zoom = '{scalePercentage}%';";
+
+            // Выполняем скрипт внутри WebBrowser
+            webBrowser.InvokeScript("execScript", new Object[] { script, "JavaScript" });
+        }
+    }
+}
+    
+
---
+++ Hall management/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hall_management
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Hall_mngmt01Entities db = new Hall_mngmt01Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Test ver. 00.01", "FAQ", MessageBoxButton.OK, MessageBoxImage.None);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = LoginBox.Text;
+            string password = PassBox.Password;
+
+            // Проверка аутентификации пользователя
+            if (db.AuthenticateUser(username, password))
+            {
+                // Пользователь успешно аутентифицирован, выполняем нужные действия
+                MessageBox.Show("Вы успешно вошли в систему!");
+                // Создаем новое окно A1_Scenario и открываем его
+                A1_Scenario a1_Scenario = new A1_Scenario();
+                a1_Scenario.Show();
+
+                // Закрываем текущее окно
+                this.Close();
+            }
+            else
+            {
+                // Пользователь не аутентифицирован, выводим сообщение об ошибке
+                MessageBox.Show("Ошибка аутентификации. Проверьте свои учетные данные.");
+            }
+        }
+    }
+}
